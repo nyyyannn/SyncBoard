73b6e3c18dc418cee418c0bddd7a557836e955e3
nyyyannn <nayanshekartg@gmail.com>
2025-06-16 18:30:03 +0530

    added a frontend folder with tailwind for css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-green-300 m-4">Welcome to SyncBoard!</h1>
+    </div>
+  )
+}
+
+export default App
