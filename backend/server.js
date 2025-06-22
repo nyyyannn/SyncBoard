d2661e9f2822c2468c62dc5ea38f61ed02a4757d
nyyyannn <nayanshekartg@gmail.com>
2025-06-22 23:54:22 +0530

    renamed index to server, shifted db connection to seperate folder

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const morgan = require("morgan");
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
-//this is only a test endpoint which will be used removed later
-app.get('/ping', (req, res) => {
-  res.send('pong 🏓');
-});
+const mongoose = require('mongoose');
+connectDB();
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
