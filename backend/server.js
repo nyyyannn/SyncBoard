d3a03e5a8a1e7fd801e14140906bae3140a7cb2f
nyyyannn <nayanshekartg@gmail.com>
2025-07-14 15:53:54 +0530

    fix: changed the path from middleware to utils

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require("morgan");
 const connectDB = require('./config/db');
-const errorMiddleware = require('./middlewares/error-middleware');
+const errorMiddleware = require('./utils/error-middleware');
 const authRoutes = require('./routes/auth-route');
 const docsRoutes = require('./routes/docs-route');
 
