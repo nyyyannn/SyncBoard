2d13c8f158a3816b21f0cea13ac68fe0db415132
nyyyannn <nayanshekartg@gmail.com>
2025-07-14 19:54:27 +0530

    removed liveblocks.config.js

--- liveblocks.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import("@liveblocks/node").LiveblocksConfig} */
-module.exports = {
-  // This tells Liveblocks where to POST to when a client requests authentication
-  authEndpoint: "/api/liveblocks/auth",
-};
