92e1fa03cb9223d649d5387a0ef81c21c51b5080
nyyyannn <nayanshekartg@gmail.com>
2025-07-15 16:43:21 +0530

    added the route to get docVersions

--- backend/routes/docs-route.js
+++ backend/routes/docs-route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth-middleware");
 const createDocSchema = require("../validators/document-validator");
-const { createDoc, getDocById, getAllDocs, inviteCollaborator, deleteDocById } = require("../controllers/docs-controller");
+const { createDoc, getDocById, getAllDocs, inviteCollaborator, deleteDocById, getDocVersions } = require("../controllers/docs-controller");
 const validate = require("../middlewares/validate-middleware");
 
 router.get("/", authMiddleware, getAllDocs);
@@ -10,5 +10,6 @@ router.post("/create", authMiddleware, validate(createDocSchema), createDoc);
 router.get("/:id", authMiddleware, getDocById);
 router.post("/:id/invite", authMiddleware, inviteCollaborator);
 router.delete("/:id",authMiddleware, deleteDocById);
+router.get("/:id/versions",authMiddleware,getDocVersions);
 
 module.exports = router;
