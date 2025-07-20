f2c6c234ae2d4ca8a48c04be60ec1c057e174ad0
nyyyannn <nayanshekartg@gmail.com>
2025-07-21 00:48:17 +0530

    added the dashboard to the routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Editor from "../pages/Editor";
-
+import Dashboard from "../pages/DashBoard";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +10,7 @@ const AppRoutes = () => {
         <Routes>
             <Route path='/login' element={<Login/>}/>
             <Route path='/signup' element={<Signup/>}/>
+            <Route path='/dashboard' element={<Dashboard/>}/>
             <Route path='/editor/:id' element={<Editor/>}/>
         </Routes>
     </Router>
