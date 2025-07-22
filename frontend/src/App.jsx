852c48de2a145965e8dc77fb8277bcfd920775d2
nyyyannn <nayanshekartg@gmail.com>
2025-07-23 04:27:16 +0530

    added Liveblocks to the react tree (wrapped the components with the liveblocksprovider component)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import AppRoutes from "./routes/AppRoutes"
+import { LiveblocksProvider } from "@liveblocks/react"
 
 const App = () => {
   return (
-    <div>
+    <LiveblocksProvider publicApiKey={"pk_dev_hcuF-yVqgy31Xmt0-KijqIJ0xUTsqt8vXTUdPX2yRhcimqY_7nFjqN7UZB4-rWDZ"}>
       <AppRoutes></AppRoutes>
-    </div>
+    </LiveblocksProvider>
   )
 }
 
