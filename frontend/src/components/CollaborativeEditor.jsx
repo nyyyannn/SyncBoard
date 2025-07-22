10441436221e9ae258a27837425cc0e31e1727a7
nyyyannn <nayanshekartg@gmail.com>
2025-07-23 03:33:22 +0530

    updated collaborativeEditor (will include slate in further udpates)

--- frontend/src/components/CollaborativeEditor.jsx
+++ frontend/src/components/CollaborativeEditor.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useMyPresence } from "@liveblocks/react/suspense";
 
 const CollaborativeEditor = () => {
+
+  const [myPresence, updateMyPresence] = useMyPresence();
+
+  useEffect(()=>{
+    console.log("My current presence:", myPresence);
+  },[myPresence]);
+
   return (
-    <div>CollaborativeEditor</div>
+    <>
+      <p><strong>Next Step:</strong> Build the Slate.js editor here.</p>
+      <p>Your name in presence: <strong>{myPresence.userInfo.name}</strong></p>
+    </>
   )
 }
 
