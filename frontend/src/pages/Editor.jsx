e28089a2cb2a121290de424a3a3b0cc52a9e642a
nyyyannn <nayanshekartg@gmail.com>
2025-07-23 03:32:36 +0530

    set the base for establishing connection in editor

--- frontend/src/pages/Editor.jsx
+++ frontend/src/pages/Editor.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import CollaborativeEditor from "../components/CollaborativeEditor";
+import { useParams } from "react-router-dom";
+import { RoomProvider } from "@liveblocks/react/suspense";
+import {useAuth} from "../context/AuthContext";
 
 const Editor = () => {
+  const {id: docId} = useParams(); //assigning the id from the URL to docId
+  const { user } = useAuth();
+
+  const publicAPI = "pk_dev_hcuF-yVqgy31Xmt0-KijqIJ0xUTsqt8vXTUdPX2yRhcimqY_7nFjqN7UZB4-rWDZ";
+
+  const userColor = "#"+Math.floor(Math.random()*16777215).toString(16); //.toString() converts it to hexadecimal format
+
+  if(!user){
+    return(
+      <div>Loading user information....</div>
+    )
+  }
+
   return (
-    <div>Editor</div>
+    <RoomProvider 
+    id={docId}
+    publicApiKey={publicAPI}
+    
+    initialPresence={{
+      cursor: null,
+      userInfo:{
+        name:user.username||"Anonymous",
+        color:userColor,
+      },
+    }}>
+
+    {/* Children of RoomProvider have access to the real-time room */}
+
+    <div>
+      <h1>Editing Document: {docId}</h1>
+      <p>You are now connected to the live room for this document.</p>
+      <CollaborativeEditor/>
+    </div>
+    </RoomProvider>
   )
 }
 
-export default Editor
+export default Editor;
